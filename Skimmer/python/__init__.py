399311e581b035e436a80a4c4b01bf610ce1da3b
Diego Figueiredo <dmf@cern.ch>
2021-05-14 10:02:07 +0200

    Cleaning.

--- Skimmer/python/__init__.py
+++ Skimmer/python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/MissingMass/Skimmer/',1)[0])+'/cfipython/slc7_amd64_gcc700/MissingMass/Skimmer')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/PPSFramework/Skimmer/',1)[0])+'/cfipython/slc7_amd64_gcc700/PPSFramework/Skimmer')
